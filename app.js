028a765b39b78fc87717cc65fc539a5f2730ab55
michaellama <micheal@superlama.com>
2020-11-17 15:24:45 -0800

    Empty Canva

---
+++ app.js
@@ -0,0 +1,20 @@
+//jshint eservices:6
+
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.listen("3000", function() {
+    console.log("Server is ready to work on port 3000");
+});
